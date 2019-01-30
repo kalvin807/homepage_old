5f96ada6a9b5157d61c9ceca0652048f6016d686
kalvin807 <kalvin807@gmail.com>
2019-01-30 21:12:31 +0800

    Updates

---
+++ precache-manifest.4940b0aa4ffacfde8062531dc50f2b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "33bcfbb74fa3cd2936c1",
+    "url": "/homepage/static/js/main.33bcfbb7.chunk.js"
+  },
+  {
+    "revision": "7232d884949943a144c3",
+    "url": "/homepage/static/js/1.7232d884.chunk.js"
+  },
+  {
+    "revision": "33bcfbb74fa3cd2936c1",
+    "url": "/homepage/static/css/main.5070821c.chunk.css"
+  },
+  {
+    "revision": "ad17b50080e0713a3350bc1e8d645de2",
+    "url": "/homepage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.dd90a6e6b264c0f58fb2f3d99b40b0e6.js"
+  "/homepage/precache-manifest.4940b0aa4ffacfde8062531dc50f2b6f.js"
 );
 
 workbox.clientsClaim();
