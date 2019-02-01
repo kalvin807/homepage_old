dbf3f7d3d14db97f8ff3bea075eac265a96dbb93
kalvin807 <kalvin807@gmail.com>
2019-02-01 14:07:19 +0800

    Updates

--- precache-manifest.28d0e80abc95f714e57d3f23a17e64c8.js
+++ precache-manifest.28d0e80abc95f714e57d3f23a17e64c8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/homepage/static/js/runtime~main.c9f2de54.js"
   },
   {
-    "revision": "127acc3e05fd3f3ad491",
-    "url": "/homepage/static/js/main.127acc3e.chunk.js"
+    "revision": "5f726c74859019e2b9a7",
+    "url": "/homepage/static/js/main.5f726c74.chunk.js"
   },
   {
     "revision": "ec999a8d08b4d4ffd1c0",
     "url": "/homepage/static/js/1.ec999a8d.chunk.js"
   },
   {
-    "revision": "127acc3e05fd3f3ad491",
-    "url": "/homepage/static/css/main.c58904c4.chunk.css"
+    "revision": "5f726c74859019e2b9a7",
+    "url": "/homepage/static/css/main.c15b351e.chunk.css"
   },
   {
-    "revision": "c8d4a1c1fa61e5c2afdda791e9cacbd3",
+    "revision": "fec2121156fd9a32f85ef09ba721ed96",
     "url": "/homepage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.8c583e87f9ef3eef48a3bda5033140e5.js"
+  "/homepage/precache-manifest.28d0e80abc95f714e57d3f23a17e64c8.js"
 );
 
 workbox.clientsClaim();
