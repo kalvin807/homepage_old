25013f2432f825ef128f71f45f245adac9377621
kalvin807 <kalvin807@gmail.com>
2019-02-01 13:55:03 +0800

    Added lint and prettier

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import { day, night } from 'contexts/themes/themes';
 import themesContext from 'contexts/themes/themesContext';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faSun, faMoon, faAngleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSun,
+  faMoon,
+  faAngleDown,
+  faAngleDoubleUp
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab , faSun, faMoon, faAngleDown, faAngleDoubleUp);
+library.add(fab, faSun, faMoon, faAngleDown, faAngleDoubleUp);
 
 class App extends Component {
   constructor(props) {
@@ -18,32 +23,28 @@ class App extends Component {
 
     this.toggleTheme = () => {
       this.setState(state => ({
-        theme:
-          state.theme === night
-            ? day
-            : night,
+        theme: state.theme === night ? day : night
       }));
     };
 
     this.state = {
       theme: day,
-      toggleTheme: this.toggleTheme, 
+      toggleTheme: this.toggleTheme
     };
   }
 
   render() {
     return (
       <div className="landing-page">
-      <themesContext.Provider value={this.state}>
-      
-            <Background/>
-            <Navbar />
-            <Landing/>
-            <About/>
-            <Projects/>
-      </themesContext.Provider>
+        <themesContext.Provider value={this.state}>
+          <Background />
+          <Navbar />
+          <Landing />
+          <About />
+          <Projects />
+        </themesContext.Provider>
       </div>
-      );
+    );
   }
 }
 
