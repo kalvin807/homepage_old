e06678c342ec96ad34ed7d2d9a9d2bcae036e9d0
Calvin Leung <kalvin807@gmail.com>
2020-01-23 16:45:55 +0800

    Updates

---
+++ precache-manifest.d8ef9d06fe35f3dcc3f1357c3382467a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b9fa3f2b195a0c2a4a6fa7f3d7cb8c",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "fe3e1ff1ade3abff55cb",
+    "url": "/homepage/static/css/main.a02aeb4d.chunk.css"
+  },
+  {
+    "revision": "8b0ef9275e9bf6ec1c75",
+    "url": "/homepage/static/js/2.aa14d51c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/homepage/static/js/2.aa14d51c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe3e1ff1ade3abff55cb",
+    "url": "/homepage/static/js/main.6c7fdbb1.chunk.js"
+  },
+  {
+    "revision": "d2aff02b19d17c103d02",
+    "url": "/homepage/static/js/runtime-main.4deff6df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.e516868a25c084bd3592d091ffb265a4.js"
+  "/homepage/precache-manifest.d8ef9d06fe35f3dcc3f1357c3382467a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/homepage/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
